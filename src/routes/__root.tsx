e67f4012560443828100fb88789c898cc98e9eb2
Raheem Adebayo <adraheemzy@gmail.com>
2025-08-26 11:50:42 +0100

    fix tanstack

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,4 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-import { TanstackDevtools } from '@tanstack/react-devtools';
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 
 import type { QueryClient } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner.tsx';
@@ -16,18 +12,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <>
       <Outlet />
       <Toaster position="top-center" richColors theme="light" />
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
     </>
   ),
 });
