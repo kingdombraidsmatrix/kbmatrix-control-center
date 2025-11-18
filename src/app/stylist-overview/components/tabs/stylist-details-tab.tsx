9867f2bcf51e3405430539c3e3b87b034b176772
Raheem Adebayo <adraheemzy@gmail.com>
2025-11-18 21:59:23 +0100

    stylist details

--- src/app/stylist-overview/components/tabs/stylist-details-tab.tsx
+++ src/app/stylist-overview/components/tabs/stylist-details-tab.tsx
@@ -1,8 +1,60 @@
-import type {Stylist} from '@/types';
+import { Mail, MapPin, PhoneCall } from 'lucide-react';
+import type { Stylist } from '@/types';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import { formatPhoneNumber } from '@/utils';
 
 interface StylistDetailsTabProps {
   stylist: Stylist;
 }
 export function StylistDetailsTab({ stylist }: StylistDetailsTabProps) {
-  return <div></div>;
+  return (
+    <div className="grid gap-6 pt-2">
+      <div className="grid gap-6 md:grid-cols-2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Contact Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-1">
+              <a
+                href="https://maps.google.com"
+                target="_blank"
+                className="flex items-center p-4 gap-6 bg-muted rounded-md border border-transparent hover:border-primary/30"
+              >
+                <MapPin className="size-5" />
+                <div className="flex-1">
+                  <p className="font-medium">{stylist.contact.address?.formattedAddress}</p>
+                  <p className="text-sm text-muted-foreground">
+                    [{stylist.contact.address?.latitude}, {stylist.contact.address?.longitude}]
+                  </p>
+                </div>
+              </a>
+              <a
+                href={`tel:${stylist.contact.phoneNumber}`}
+                target="_blank"
+                className="flex items-center p-4 gap-6 bg-muted rounded-md border border-transparent hover:border-primary/30"
+              >
+                <PhoneCall className="size-5" />
+                <div className="flex-1">
+                  <p className="font-medium">{formatPhoneNumber(stylist.contact.phoneNumber)}</p>
+                  <p className="text-sm text-muted-foreground">Business Phone Number</p>
+                </div>
+              </a>
+              <a
+                href={`mailto:${stylist.contact.email}`}
+                target="_blank"
+                className="flex items-center p-4 gap-6 bg-muted rounded-md border border-transparent hover:border-primary/30"
+              >
+                <Mail className="size-5" />
+                <div className="flex-1">
+                  <p className="font-medium">{stylist.contact.email}</p>
+                  <p className="text-sm text-muted-foreground">Business Email</p>
+                </div>
+              </a>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
