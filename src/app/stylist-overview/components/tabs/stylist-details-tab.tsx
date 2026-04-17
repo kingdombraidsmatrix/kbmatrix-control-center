6278b4427094bc644603cfaba7f6f9d8b6a7f932
Raheem Adebayo <adraheemzy@gmail.com>
2026-04-17 14:43:14 +0100

    chore: fix address latitude and longitude

--- src/app/stylist-overview/components/tabs/stylist-details-tab.tsx
+++ src/app/stylist-overview/components/tabs/stylist-details-tab.tsx
@@ -22,7 +22,7 @@ export function StylistDetailsTab({ stylist }: StylistDetailsTabProps) {
               <a
                 href={
                   stylist.contact.address
-                    ? `https://www.google.com/maps/place/${stylist.contact.address.longitude},${stylist.contact.address.latitude}`
+                    ? `https://www.google.com/maps/place/${stylist.contact.address.latitude},${stylist.contact.address.longitude}`
                     : ''
                 }
                 target="_blank"
